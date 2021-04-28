ab24ed0294a3835286e10aee9da4d846a5bcffcf
admin <admin@zxjdeMac.local>
2021-04-28 18:30:07 +0800

    jiayan

--- src/components/content/Company.vue
+++ src/components/content/Company.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="msearch">
-        <div class="search-cell">
-            <table>
-                <tr>
-                    <td><span>上海佳研实业有限公司</span></td>
-                    <td>ShanghaiJiaYanShiYe</td>
-                    <td>性质：</td>
-                    <td>城市：上海</td>
-                    <td>注册地址：</td>
-                </tr>
-                <tr>2</tr>
-                <tr>3</tr>
-            </table>
-        </div>
+        <el-row>
+            <el-col :xs="16" :sm="16" :offset="4">
+                <div class="search-cell">
+                    <table>
+                        <tr>
+                            <td>
+                                <h3>上海佳研实业有限公司</h3>
+                                <div class="rline">
+                                    <span>ShanghaiJiaYanShiYe</span>
+                                    <span>性质:</span>
+                                    <span>城市:上海市</span>
+                                </div>
+                                <div class="rline">
+                                    <span>注册地址：上海市徐汇区上海五洲国际大厦</span>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
-import API from '@/api/index'
+// import API from '@/api/index'
 
 export default {
     name: "Company",
@@ -26,13 +34,13 @@ export default {
             companies: []
         }
     },
-    created() {
-        API.getCompanies().then(res => {
-            console.log(res.data);
-        }).catch(err => {
-            console.log(err);
-        })
-    }
+    // created() {
+    //     API.getCompanies().then(res => {
+    //         console.log(res.data);
+    //     }).catch(err => {
+    //         console.log(err);
+    //     })
+    // }
 }
 </script>
 
@@ -40,20 +48,30 @@ export default {
     $margin-auto: 0 auto;
     .msearch {
         text-align: center;
+        font-size: 1rem;
     }
     .search-cell {
-        width: 80%;
         margin: $margin-auto;
         table {
             width: 100%;
-            background-color: transparent;
             border: 1px solid grey;
             border-collapse: collapse;
             tr {
-                height: 200px;
+                height: 100px;
                 border-top: 1px solid grey;
                 border-bottom: 1px solid grey;
+                td {
+                    text-align: left;
+                    padding-left: 5%;
+                    padding-bottom: 2%;
+                }
             }
+        }
+    }
+    .rline {
+        height: 25px;
+        span {
+            margin-right: 20%;
         }
     }
 </style>
